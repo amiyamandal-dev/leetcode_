981c32318a6008d260f276edc0b040a5d20dad68
amiya <amiya19mandal@gmail.com>
2026-01-13 12:39:54 +0530

    Refactor addTwoNumbers function to improve readability and handle carry for linked list addition

--- src/add-two-numbers.zig
+++ src/add-two-numbers.zig
@@ -26,29 +26,30 @@ const ListNode = struct {
 
 const Solution = struct {
     pub fn addTwoNumbers(l1: ?*ListNode, l2: ?*ListNode, allocator: std.mem.Allocator) !?*ListNode {
-        if (l1) |l1_node| if (l2) |l2_node| {
-            var sum_val = l1_node.val + l2_node.val;
+        var dummy = ListNode{ .val = 0, .next = null };
+        var current: *ListNode = &dummy;
+        var carry: i32 = 0;
+        var p1 = l1;
+        var p2 = l2;
 
-            const head = try ListNode.init(allocator, sum_val);
-            var current = head;
-
-            while (l1_node.next) |node| {
-                if (l2_node.next) |node2| {
-                    sum_val = node.val + node2.val;
-                    const new_node = try ListNode.init(allocator, sum_val);
-                    current.next = new_node;
-                    current = new_node;
-                    l2_node = node2.next;
-                }
+        while (p1 != null or p2 != null or carry != 0) {
+            var sum = carry;
+            if (p1) |node| {
+                sum += node.val;
+                p1 = node.next;
             }
-            return head;
-        } else {
-            return null;
-        } else {
-            return null;
+            if (p2) |node| {
+                sum += node.val;
+                p2 = node.next;
+            }
+            carry = @divFloor(sum, 10);
+            const digit = @rem(sum, 10);
+            const new_node = try ListNode.init(allocator, digit);
+            current.next = new_node;
+            current = new_node;
         }
 
-        return null;
+        return dummy.next;
     }
 };
 
@@ -57,10 +58,9 @@ fn createList(allocator: std.mem.Allocator, values: []const i32) !?*ListNode {
     if (values.len == 0) return null;
     const head = try ListNode.init(allocator, values[0]);
     var current = head;
-    for (values, 1..) |val, index| {
-        _ = index;
+    for (values[1..]) |val| {
         const new_node = try ListNode.init(allocator, val);
-        current.node = new_node;
+        current.next = new_node;
         current = new_node;
     }
 
@@ -69,19 +69,14 @@ fn createList(allocator: std.mem.Allocator, values: []const i32) !?*ListNode {
 
 // Helper function to convert linked list to array for comparison
 fn listToArray(allocator: std.mem.Allocator, head: ?*ListNode) ![]i32 {
-    if (head) |head_node| {
-        var list = std.ArrayListUnmanaged(i32){};
-        var current = head_node;
-        while (current) |node| {
-            try list.append(allocator, node.val);
-            current = current.node;
-        }
-        const arr: []const i32 = list.items;
-
-        return &arr;
-    } else {
-        return &[_]i32{};
+    var list = std.ArrayListUnmanaged(i32){};
+    defer list.deinit(allocator);
+    var current = head;
+    while (current) |n| {
+        try list.append(allocator, n.val);
+        current = n.next;
     }
+    return try list.toOwnedSlice(allocator);
 }
 
 // Helper function to free linked list
@@ -101,15 +96,18 @@ test "example 1: l1 = [2,4,3], l2 = [5,6,4], output = [7,0,8]" {
     const l2_values = [_]i32{ 5, 6, 4 };
     const expected = [_]i32{ 7, 0, 8 };
 
-    // TODO: Create linked lists from arrays
-    // TODO: Call addTwoNumbers
-    // TODO: Convert result to array and compare with expected
-    // TODO: Free all allocated memory
+    const l1 = try createList(allocator, &l1_values);
+    defer freeList(allocator, l1);
+    const l2 = try createList(allocator, &l2_values);
+    defer freeList(allocator, l2);
 
-    _ = l1_values;
-    _ = l2_values;
-    _ = expected;
-    _ = allocator;
+    const result = try Solution.addTwoNumbers(l1, l2, allocator);
+    defer freeList(allocator, result);
+
+    const result_arr = try listToArray(allocator, result);
+    defer allocator.free(result_arr);
+
+    try std.testing.expectEqualSlices(i32, &expected, result_arr);
 }
 
 test "example 2: l1 = [0], l2 = [0], output = [0]" {
@@ -121,15 +119,18 @@ test "example 2: l1 = [0], l2 = [0], output = [0]" {
     const l2_values = [_]i32{0};
     const expected = [_]i32{0};
 
-    // TODO: Create linked lists from arrays
-    // TODO: Call addTwoNumbers
-    // TODO: Convert result to array and compare with expected
-    // TODO: Free all allocated memory
+    const l1 = try createList(allocator, &l1_values);
+    defer freeList(allocator, l1);
+    const l2 = try createList(allocator, &l2_values);
+    defer freeList(allocator, l2);
 
-    _ = l1_values;
-    _ = l2_values;
-    _ = expected;
-    _ = allocator;
+    const result = try Solution.addTwoNumbers(l1, l2, allocator);
+    defer freeList(allocator, result);
+
+    const result_arr = try listToArray(allocator, result);
+    defer allocator.free(result_arr);
+
+    try std.testing.expectEqualSlices(i32, &expected, result_arr);
 }
 
 test "example 3: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9], output = [8,9,9,9,0,0,0,1]" {
@@ -141,13 +142,16 @@ test "example 3: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9], output = [8,9,9,9,0,0,0,1
     const l2_values = [_]i32{ 9, 9, 9, 9 };
     const expected = [_]i32{ 8, 9, 9, 9, 0, 0, 0, 1 };
 
-    // TODO: Create linked lists from arrays
-    // TODO: Call addTwoNumbers
-    // TODO: Convert result to array and compare with expected
-    // TODO: Free all allocated memory
+    const l1 = try createList(allocator, &l1_values);
+    defer freeList(allocator, l1);
+    const l2 = try createList(allocator, &l2_values);
+    defer freeList(allocator, l2);
 
-    _ = l1_values;
-    _ = l2_values;
-    _ = expected;
-    _ = allocator;
+    const result = try Solution.addTwoNumbers(l1, l2, allocator);
+    defer freeList(allocator, result);
+
+    const result_arr = try listToArray(allocator, result);
+    defer allocator.free(result_arr);
+
+    try std.testing.expectEqualSlices(i32, &expected, result_arr);
 }
